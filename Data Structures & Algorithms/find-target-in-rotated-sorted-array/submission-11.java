cd64fab355fa5e1b255c93924e6cd5e724a5a5f6
Sohan <sohanz.singh2@gmail.com>
2026-08-02 02:31:40 +0530

    Add: find-target-in-rotated-sorted-array - submission-11

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-11.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low ) / 2;
+            if (target == nums[mid])
+                return mid;
+            if (nums[mid] >= nums[low]) {        // left array is sorted
+                if (target <= nums[mid] && target >= nums[low]) {       // target is less than mid element and greater than first element
+                    high = mid - 1;
+                } else low = mid + 1;
+            } else {                            // right array is sorted
+                if (target >= nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
